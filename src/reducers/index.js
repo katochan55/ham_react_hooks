d66e1f41dbcba8b021a7c70d6f38d58f5b128135
kodai-kato <greatvoyage0122@gmail.com>
2020-12-01 22:56:54 +0900

    Create reducer for events

---
+++ src/reducers/index.js
@@ -0,0 +1,50 @@
+// action = {
+//   type: 'CREATE_EVENT',
+//   title: '2020東京オリンピックのお知らせ',
+//   body: '2020年に東京でオリンピックを開催します。つきましては、、、',
+// }
+//
+// ケース1
+// #before
+// state = []
+//
+// #after
+// state = [
+//   { id: 1, title: '2020東京オリンピックのお知らせ', body: '2020年に東京でオリンピックを開催します。つきましては、、、'},
+// ]
+//
+// ケース2
+// #before
+// state = [
+//   {id: 1, title: 'タイトル1', body: 'ボディー1'},
+//   {id: 2, title: 'タイトル2', body: 'ボディー2'},
+//   {id: 3, title: 'タイトル3', body: 'ボディー3'},
+// ]
+//
+// #after
+// state = [
+//   {id: 1, title: 'タイトル1', body: 'ボディー1'},
+//   {id: 2, title: 'タイトル2', body: 'ボディー2'},
+//   {id: 3, title: 'タイトル3', body: 'ボディー3'},
+//   {id: 4, title: '2020東京オリンピックのお知らせ', body: '2020年に東京でオリンピックを開催します。つきましては、、、'},
+// ]
+
+// stateが未定義だった場合に備え[]を代入しておく
+const events = (state = [], action) => {
+  switch(action.type) {
+    case 'CREATE_EVENT':
+      const event = { title: action.title, body: action.body }
+      const length = state.length;
+      const id = length === 0 ? 1 : state[length - 1].id + 1;
+      return [...state, { id, ...event }];  // id: idのショートハンド
+    case 'DELETE_EVENT':
+      return state;
+    case 'DELETE_ALL_EVENT':
+      return [];
+    default:
+      return state;
+  }
+}
+
+// componentで使うためexport
+export default events;
