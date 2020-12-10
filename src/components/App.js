237667d20928d0112fd38c748875aceb00aeb4f9
kodai-kato <greatvoyage0122@gmail.com>
2020-12-10 22:17:34 +0900

    Refactor reducers

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,10 @@ import AppContext from '../contexts/AppContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, []);
+  const initialState = {
+    events: []
+  }
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <AppContext.Provider value={{ state, dispatch }}>
